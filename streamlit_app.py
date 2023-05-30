8edc0d932e278e0ef705d6998708b32d524eba55
MeetDesai25 <135005668+MeetDesai25@users.noreply.github.com>
2023-05-30 13:31:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+import numpy as np
 
 """
 # Welcome to Streamlit!
@@ -15,24 +16,17 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+URL = st.input("Enter Squad URL")
+def func_run(URL):
+  import requests
+  from bs4 import BeautifulSoup
+  import pandas as pd
+  import re
+  import numpy as np
+   
+  page = requests.get(URL)
+  bs = BeautifulSoup(page.content, 'lxml')
+  y =bs.find_all('span',class_='ds-text-title-m ds-font-bold ds-text-typo ds-underline ds-decoration-ui-stroke hover:ds-text-typo-primary hover:ds-decoration-ui-stroke-primary ds-block')
+  for y1 in y:
+    st.write(y1.get_text())
+func_run(URL)
