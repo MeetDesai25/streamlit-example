67ca0dfc764c90bcecac9559e87ed51d5700a269
meetdesai25 <135005668+meetdesai25@users.noreply.github.com>
2023-07-24 12:34:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ if URL:
 
     page = requests.get(URL)
     bs = BeautifulSoup(page.content, 'lxml')
-    y =bs.find_all('span',class_='ds-text-title-m ds-font-bold ds-text-typo ds-underline ds-decoration-ui-stroke hover:ds-text-typo-primary hover:ds-decoration-ui-stroke-primary ds-block')
+    y =bs.find_all('span',class_='ds-text-compact-s ds-font-bold ds-text-typo ds-underline ds-decoration-ui-stroke hover:ds-text-typo-primary hover:ds-decoration-ui-stroke-primary ds-block')
     st.title("Squad:")
     for y1 in y:
       st.write(y1.get_text())
